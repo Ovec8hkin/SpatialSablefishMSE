004b5d43a2848e87b1526e74a0226dc9cea0e02f
Joshua Zahner <joshuazahner@gmail.com>
2024-03-19 01:22:59 -0700

    feat: Support for NPFMC Tier 3a HCR
    Function to simulate the NPFMC Tier 3a groundfish HCR

---
+++ R/harvest_control_rules.R
@@ -0,0 +1,13 @@
+npfmc_tier3a_F <- function(ssb, fmax){
+    lrp <- 0.05
+    urp <- 1.0
+    F <- 0.0
+    if(ssb >= urp){
+        F <- fmax
+    }else if(lrp < ssb && ssb < urp){
+        F <- fmax * (ssb-lrp)/(urp-lrp)
+    }
+    return(F)
+}
+
+# npfmc_tier3a_F(ssb=10, fmax=F40)
