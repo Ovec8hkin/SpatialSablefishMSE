a4da99187450688bc6cd13439abdb28bb2a22d44
Matt Cheng <84427627+chengmatt@users.noreply.github.com>
2024-03-19 21:53:06 -0800

    add capped control rule

--- R/harvest_control_rules.R
+++ R/harvest_control_rules.R
@@ -7,3 +7,10 @@ npfmc_tier3_F <- function(ssb, b40, f40, alpha){
 }
 
 # npfmc_tier3a_F(ssb=10, fmax=F40)
+
+threshold_cap <- function(ssb,b40,f40,alpha) {
+  if(ssb/b40 >= 1)  F <- f40  / (ssb/b40)  # if stock status >= 1
+  if(ssb/b40 > alpha && ssb/b40 < 1) F <- f40 * ((ssb/b40)-alpha)/(1-alpha) # if stock status > alpha & stock status < 1
+  if(ssb/b40 <= alpha) F <- 0 # if stock stats <= alpha
+  return(F)
+}
