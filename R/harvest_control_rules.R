4f3fcbb9ebccadeafe8458aabd218969a7e7aaed
Matt Cheng <84427627+chengmatt@users.noreply.github.com>
2024-03-20 08:03:49 -0800

    roxygen for hcr

--- R/harvest_control_rules.R
+++ R/harvest_control_rules.R
@@ -1,16 +1,35 @@
-npfmc_tier3_F <- function(ssb, b40, f40, alpha){
-    if(ssb/b40 >= 1)  F <- f40 # if stock status >= 1
-    if(ssb/b40 > alpha && ssb/b40 < 1) F <- f40 * ((ssb/b40)-alpha)/(1-alpha) # if stock status > alpha & stock status < 1
-    if(ssb/b40 <= alpha) F <- 0 # if stock stats <= alpha
-    
+#' Title Compute F from Tier 3 HCR
+#'
+#' @param ssb Value for SSB
+#' @param B_ref Value for biomass based reference point (e.g., B40)
+#' @param F_ref Value for fishing mortality based reference point (e.g., F40)
+#' @param alpha Cutoff to set F at 0
+#'
+#' @return
+#' @export
+#'
+#' @examples
+npfmc_tier3_F <- function(ssb, B_ref, F_ref, alpha){
+    if(ssb/B_ref >= 1)  F <- F_ref # if stock status >= 1
+    if(ssb/B_ref > alpha && ssb/B_ref < 1) F <- F_ref * ((ssb/B_ref)-alpha)/(1-alpha) # if stock status > alpha & stock status < 1
+    if(ssb/B_ref <= alpha) F <- 0 # if stock stats <= alpha
     return(F)
 }
 
-# npfmc_tier3a_F(ssb=10, fmax=F40)
-
-threshold_cap <- function(ssb,b40,f40,alpha) {
-  if(ssb/b40 >= 1)  F <- f40  / (ssb/b40)  # if stock status >= 1
-  if(ssb/b40 > alpha && ssb/b40 < 1) F <- f40 * ((ssb/b40)-alpha)/(1-alpha) # if stock status > alpha & stock status < 1
-  if(ssb/b40 <= alpha) F <- 0 # if stock stats <= alpha
+#' Title Compute F following a threshold rule, with a cap at B/B_ref = 1
+#'
+#' @param ssb Value for SSB
+#' @param B_ref Value for biomass based reference point (e.g., B40)
+#' @param F_ref Value for fishing mortality based reference point (e.g., F40)
+#' @param alpha Cutoff to set F at 0
+#'
+#' @return
+#' @export
+#'
+#' @examples
+threshold_cap <- function(ssb,B_ref,F_ref,alpha) {
+  if(ssb/B_ref >= 1)  F <- F_ref  / (ssb/B_ref)  # if stock status >= 1
+  if(ssb/B_ref > alpha && ssb/B_ref < 1) F <- F_ref * ((ssb/B_ref)-alpha)/(1-alpha) # if stock status > alpha & stock status < 1
+  if(ssb/B_ref <= alpha) F <- 0 # if stock stats <= alpha
   return(F)
 }
