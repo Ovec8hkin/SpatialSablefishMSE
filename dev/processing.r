e82bd77cedcf0fd86d7ff61e3bf9a4faae8399cd
Joshua Zahner <joshuazahner@gmail.com>
2024-03-26 16:01:09 -0700

    feat: Example usage of `format_em_data(...)` and `fit_TMB_model(...)`

--- dev/processing.r
+++ dev/processing.r
@@ -7,6 +7,7 @@ library(patchwork)
 library(tidyverse)
 library(ggdist)
 library(ggh4x)
+library(SpatialSablefishAssessment)
 
 afscOM_dir <- "~/Desktop/Projects/afscOM"
 devtools::load_all(afscOM_dir)
@@ -16,15 +17,30 @@ source("R/simulate_TAC.R")
 source("R/age_structure_stats.R")
 source("R/data_utils.R")
 source("R/data_processing.R")
+source("R/format_em_data.R")
+source("R/fit_TMB_model.R")
 
 assessment <- dget("data/sablefish_assessment_2023.rdat")
 
 # Load OM parameters into global environment
 list2env(readRDS("data/sablefish_om.RDS"), globalenv())
+model_options$obs_pars$surv_ll$ac_samps <- 50
+model_options$obs_pars$surv_ll$rpn_cv <- 0.01
+model_options$obs_pars$surv_ll$rpw_cv <- 0.01
+model_options$obs_pars$surv_ll$as_integers <- TRUE
+
+model_options$obs_pars$surv_tw$ac_samps <- 50
+model_options$obs_pars$surv_tw$rpw_cv <- 0.01
+model_options$obs_pars$surv_tw$as_integers <- TRUE
+
+model_options$obs_pars$fish_fx$ac_samps <- 50
+model_options$obs_pars$fish_fx$as_integers <- TRUE
+
+model_options$simulate_observations <- TRUE
 
 # Load OM dimensions into global environment
 list2env(afscOM::get_model_dimensions(dem_params$sel), globalenv())
-nsims <- 50
+nsims <- 1
 
 # Dimension names
 dimension_names <- list(
@@ -61,17 +77,29 @@ out_f       = array(NA, dim=c(nyears, 1, 1, 1, nsims), dimnames=list("time"=1:ny
 naa         = array(NA, dim=c(nyears+1, nages, nsexes, nregions, nsims), dimnames=list("time"=1:(nyears+1), "age"=2:31, "sex"=c("F", "M"), "region"="Alaska", "sim"=1:nsims))
 naa[1,,,,] = init_naa
 
+naa_est     = array(NA, dim=c(nyears, nages, nsexes, nregions, nsims), dimnames=list("time"=1:(nyears), "age"=2:31, "sex"=c("F", "M"), "region"="Alaska", "sim"=1:nsims))
+
+survey_obs <- list(
+    ll_rpn = array(NA, dim=c(nyears, 1, 1, nregions)),
+    ll_rpw = array(NA, dim=c(nyears, 1, 1, nregions)),
+    tw_rpw = array(NA, dim=c(nyears, 1, 1, nregions)),
+    ll_acs = array(NA, dim=c(nyears, nages, nsexes, nregions)),
+    fxfish_acs = array(NA, dim=c(nyears, nages, nsexes, nregions))
+)
+
+set.seed(1120)
 seeds <- sample(1:(nsims*1000), size=nsims, replace=FALSE)
-ref_naa <- readRDS("data/agestruct_f40.RDS")
-for(s in 1:50){
+ref_as <- readRDS("data/agestruct_f40.RDS")
+ref_naa <- ref_as$naa
+for(s in 1:nsims){
     print(s)
     set.seed(seeds[s])
     recruitment <- assessment$natage.female[,1]*2
     projected_recruitment <- sample(recruitment, size=nyears-length(recruitment)+1, replace=TRUE)
     recruitment <- c(recruitment, projected_recruitment)
 
-    for(y in 1:(nyears-1)){
-
+    for(y in 1:67){
+        print(paste("Year", y))
         # Subset the demographic parameters list to only the current year
         # and DO NOT drop lost dimensions.
         dp.y <- subset_dem_params(dem_params = dem_params, y, d=1, drop=FALSE)
@@ -98,8 +126,41 @@ for(s in 1:50){
         tac[y,,,,s] <- TACs[y]
         hcr_f[y,,,,s] <- hcr_F[y]
 
+        survey_obs$ll_rpn[y,,,] <- out_vars$surv_obs$ll_rpn
+        survey_obs$ll_rpw[y,,,] <- out_vars$surv_obs$ll_rpw
+        survey_obs$tw_rpw[y,,,] <- out_vars$surv_obs$tw_rpw
+        survey_obs$ll_acs[y,,,] <- out_vars$surv_obs$ll_ac_obs
+        survey_obs$fxfish_acs[y,,,] <- out_vars$surv_obs$fxfish_caa_obs
+
         #new_F <- 0.085
         if((y+1) > 64){
+
+            assess_inputs <- format_em_data(
+                nyears = y,
+                dem_params = dp.y,
+                land_caa = out_vars$land_caa_tmp,
+                survey_indices = out_vars$surv_obs,
+                fxfish_caa_obs = survey_obs$fxfish_acs[y-1,,,,drop=FALSE],
+                ll_ac_obs = survey_obs$ll_acs[y-1,,,,drop=FALSE],
+                model_options = model_options,
+                added_years = 1
+            )
+
+            mod_report <- fit_TMB_model(assess_inputs$new_data, assess_inputs$new_parameters)  
+            assessment_ssb <- SpatialSablefishAssessment::get_SSB(mod_report) %>% filter(Year == max(Year)) %>% pull(SSB)
+
+            if(y == 64){
+                naaf <- t(mod_report$natage_f[,1:63])
+                naam <- t(mod_report$natage_m[,1:63])
+                naa_est[1:63,,1,,s] <- naaf
+                naa_est[1:63,,2,,s] <- naam
+            }
+
+            naa_est[y,,1,,s] <- mod_report$natage_f[,y]
+            naa_est[y,,2,,s] <- mod_report$natage_m[,y]
+            
+            ###########
+
             joint_self <- apply(dp.y$sel[,,1,,,drop=FALSE], c(1, 2), sum)/max(apply(dp.y$sel[,,1,,,drop=FALSE], c(1, 2), sum))
             joint_selm <- apply(dp.y$sel[,,2,,,drop=FALSE], c(1, 2), sum)/max(apply(dp.y$sel[,,1,,,drop=FALSE], c(1, 2), sum))
             joint_ret <- apply(dp.y$ret[,,1,,,drop=FALSE], c(1, 2), sum)/max(apply(dp.y$ret[,,1,,,drop=FALSE], c(1, 2), sum))
@@ -115,18 +176,19 @@ for(s in 1:50){
                 avg_rec = mean(recruitment)/2
             )
 
-            ssb <- apply(out_vars$naa_tmp[,,1,]*dp.y$waa[,,1,,drop=FALSE]*dp.y$mat[,,1,,drop=FALSE], 1, sum)
-            hcr_F[y] <- as_scalar_threshold_f(
-                            ssb/ref_pts$B40, 
-                            naa=out_vars$naa_tmp[,,1,], 
-                            ref_naa=ref_naa,
-                            as_func = abi,
-                            ref = ref_naa,
-                            f_min=0,
-                            f_max=ref_pts$F40,
-                            lrp=0.05,
-                            urp=1.0
-                        )
+            #ssb <- apply(out_vars$naa_tmp[,,1,]*dp.y$waa[,,1,,drop=FALSE]*dp.y$mat[,,1,,drop=FALSE], 1, sum)
+            hcr_F[y] <- npfmc_tier3_F(assessment_ssb, ref_pts$B40, ref_pts$F40)
+            # hcr_F[y] <- as_scalar_threshold_f(
+            #                 ssb/ref_pts$B40, 
+            #                 naa=out_vars$naa_tmp[,,1,], 
+            #                 ref_naa=ref_naa,
+            #                 as_func = shannon_diversity,
+            #                 #ages = 2:31,
+            #                 f_min=0,
+            #                 f_max=ref_pts$F40,
+            #                 lrp=0.05,
+            #                 urp=1.0
+            #             )
 
             joint_sel <- array(NA, dim=dim(out_vars$naa_tmp))
             joint_sel[,,1,] <- joint_self
@@ -135,12 +197,27 @@ for(s in 1:50){
             TACs[y+1] <- simulate_TAC(hcr_F[y], out_vars$naa_tmp, mean(recruitment)/2, joint_sel, dp.y)
         }   
     }
+
+    file.remove("data/sablefish_em_data_curr.RDS")
+    file.remove("data/sablefish_em_par_curr.RDS")
 }
+
+
+# l <- afscOM::listN(dem_params, model_options, land_caa, caa, naa, faa, tac, hcr_f, survey_obs)
+# saveRDS(l, "data/om2.RDS")
 
 library(reshape2)
 library(ggdist)
 library(ggh4x)
 
+# sum(naa_est[70, , 1, 1,1]*dp.y$mat[,,1,1]*dp.y$waa[,,1,1])
+# sum(naa[70, , 1, 1,1])
+
+om_ssb <- apply(naa[1:100,,1,1,1]*dem_params$mat[1:100,,1,1]*dem_params$waa[1:100,,1,1], 1, sum)
+em_ssb <- apply(naa_est[1:100,,1,1,1]*dem_params$mat[1:100,,1,1]*dem_params$waa[1:100,,1,1], 1, sum)
+
+plot(1:100, om_ssb, type="l", ylim=c(0, 300))
+lines(1:100, em_ssb, col="red")
 
 pos_scales <- list(
     scale_y_continuous(limits=c(0, 300)),
@@ -183,3 +260,27 @@ ref_points_df <- data.frame(
     name = factor(c("tot_spbio", "catch", "tot_bio", "F"), levels=c("tot_spbio", "catch", "tot_bio", "F"), labels=c("Spawning Biomass (mt)", "Catch (mt)", "Total Biomass (mt)", "Summary F")),
     rp = c(final_ref_points_fem$B40, final_ref_points_tot$B40*final_ref_points_tot$F40, final_ref_points_tot$B40, final_ref_points_fem$F40)
 )
+
+
+melt(naa) %>% as_tibble() %>%
+    filter(sex == "F") %>%
+    drop_na() %>%
+    group_by(time, age, sim) %>%
+    summarise(
+        total_num = sum(value)
+    ) %>%
+    group_by(time, age) %>%
+    summarise(
+        total_num = mean(total_num)
+    ) %>%
+    mutate(
+        class = case_when(age < 7 ~ "Young", age < 16 ~ "Immature", age > 15 ~ "Mature")
+    ) %>%
+    mutate(
+        class = factor(class, levels=c("Young", "Immature", "Mature"), labels=c("Young (<7yo)", "Immature (7-15yo)", "Mature (16+yo)"))
+    ) %>%
+    
+    ggplot()+
+        geom_bar(aes(x=time, y=total_num, fill=class), position="fill", stat="identity")+
+        scale_x_continuous(breaks=seq(1, nyears+1, 20), labels=seq(1960, 1960+nyears+1, 20))+
+        coord_cartesian(expand=0)
