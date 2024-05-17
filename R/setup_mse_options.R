cc995374264477209ee614ef5e570c9e023d5aea
Joshua Zahner <joshuazahner@gmail.com>
2024-05-16 22:32:55 -0700

    Default values for `mse_options` list

---
+++ R/setup_mse_options.R
@@ -0,0 +1,26 @@
+#' Setup MSE Options List
+#' 
+#' Set MSE options list items to reasonable default values.
+#'
+#' @export setup_mse_options
+#'
+#' @example
+#'
+setup_mse_options <- function(){
+
+    return(
+        list(
+            run_estimation = TRUE,
+            ref_points = list(
+                spr_target = 0.40
+            ),
+            management = list(
+                abc_tac_reduction = 1,
+                tac_land_reduction = 1,
+                max_stability = NA,
+                harvest_cap = NA
+            )
+        )
+    )
+
+}
