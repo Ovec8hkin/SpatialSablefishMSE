dd59a285f569d7b3382ddd9f244df528fb8c32d8
Joshua Zahner <joshuazahner@gmail.com>
2024-06-25 04:15:42 -0700

    feat: Calculate common performance metrics
    Functions for calculating average catch, average biomass, aav, and average proportions of population/catch are old/large from MSE model outputs

---
+++ R/performance_metrics.R
@@ -0,0 +1,99 @@
+average_catch <- function(model_runs, extra_columns, interval_widths=c(0.50, 0.80)){
+    return(
+        bind_mse_outputs(model_runs, "caa", extra_columns) %>%
+            as_tibble() %>%
+            filter(time > 64) %>%
+            group_by(time, sim, om, hcr) %>%
+            mutate(total_catch = sum(value)) %>%
+            group_by(om, hcr) %>%
+            median_qi(total_catch, .width=interval_widths)
+    )
+}
+
+average_ssb <- function(model_runs, extra_columns, interval_widths=c(0.50, 0.80)){
+    return(
+        get_ssb_biomass(model_runs, extra_columns) %>%
+            filter(L1 == "naa_est", time > 64) %>%
+            group_by(om, hcr) %>%
+            median_qi(spbio, .width=interval_widths)
+    )
+}
+
+average_annual_catch_variation <- function(model_runs, extra_columns, interval_widths=c(0.50, 0.80)){
+    return(
+        get_landed_catch(model_runs, extra_columns) %>% 
+            filter(time > 64) %>%
+            group_by(sim, hcr, om) %>%
+            summarise(
+                aav = aav(total_catch)
+            ) %>%
+            group_by(hcr, om) %>%
+            median_qi(aav, .width=interval_widths)
+    )
+}
+
+average_proportion_catch_large <- function(model_runs, extra_columns, interval_widths=c(0.50, 0.80)){
+    return(
+        bind_mse_outputs(model_runs, "caa", extra_columns) %>%
+            as_tibble() %>%
+            filter(time > 64) %>%
+            mutate(
+                size_group = case_when(
+                    age < 5 ~ "Small",
+                    age < 9 ~ "Medium",
+                    TRUE ~ "Large"
+                )
+            ) %>%
+            group_by(time, sim, size_group, om, hcr) %>%
+            summarise(total_catch = sum(value)) %>%
+            ungroup() %>%
+            pivot_wider(names_from = "size_group", values_from="total_catch") %>%
+            rowwise() %>%
+            mutate(
+                total_catch = sum(Large, Medium, Small)
+            ) %>%
+            mutate(across(Large:Small, ~ ./total_catch)) %>%
+            select(-total_catch) %>%
+            ungroup() %>%
+            pivot_longer(Large:Small, names_to="size_group", values_to="catch") %>%
+            group_by(size_group, om, hcr) %>%
+            median_qi(catch, .width=interval_widths)
+    )
+}
+
+average_proportion_biomass_old <- function(model_runs, extra_columns, interval_widths=c(0.50, 0.80)){
+    return(
+        bind_mse_outputs(model_runs, "naa", extra_columns) %>%
+            as_tibble() %>%
+            # join WAA and maturity-at-age for computing SSB
+            left_join(
+                melt(om_list$om1$dem_params$waa, value.name="weight"), 
+                by=c("time", "age", "sex")
+            ) %>%
+            mutate(bio = value*weight) %>%
+            select(time, age, sex, sim, om, hcr, bio) %>%
+            filter(time > 64) %>%
+            mutate(
+                age_group = case_when(
+                    age < 7 ~ "Young",
+                    age < 21 ~ "Adult",
+                    TRUE ~ "Old"
+                )
+            ) %>%
+            group_by(time, sim, age_group, om, hcr) %>%
+            summarise(total_bio = sum(bio)) %>%
+            ungroup() %>%
+            pivot_wider(names_from = "age_group", values_from="total_bio") %>%
+            rowwise() %>%
+            mutate(
+                total_bio = sum(Young, Adult, Old)
+            ) %>%
+            mutate(across(Adult:Young, ~ ./total_bio)) %>%
+            select(-total_bio) %>%
+            ungroup() %>%
+            pivot_longer(Adult:Young, names_to="age_group", values_to="bio") %>%
+            group_by(age_group, om, hcr) %>%
+            median_qi(bio, .width=interval_widths) %>%
+            arrange(.width, hcr, om, age_group)
+    )
+}
--- R/utils.R
+++ R/utils.R
@@ -28,3 +28,65 @@ calculate_joint_selret <- function(sel, ret, prop_fs=c(0.50, 0.50)){
 
     return(list(sel=joint_sel, ret=joint_ret))
 }
+
+
+#' Create dplyr Filter for Years Around Large Recruitment Events
+#' 
+#' Given a list of MSE model runs, identifies years in which recruitment was
+#' larger than a specified threshold value for each OM simulation. That information
+#' is used to generate an complex boolean expression object for use with 
+#' dplyr::filter(...) when computing performance metrics.
+#'
+#' @param om_list a list of OM objects (as would be given to `run_mse_multiple`)
+#' @param seed_list a vector or list of simulation seeds (as would be given to `run_mse_multiple`)
+#' @param model_runs list of completed MSE model runs (the output of `run_mse_multiple`)
+#' @param large_event_thresh threshold value for declaring a recruitment event "large"
+#'
+#' @export 
+#'
+#' @example
+#'
+get_big_recruitment_filter <- function(om_list, seed_list, model_runs, large_event_thresh){
+    rec_years_interest <- list()
+
+    for(o in 1:length(om_list)){
+        oname <- names(om_list)[o]
+        mr1 <- model_runs[[o]]
+        for(s in 1:length(seed_list)){
+            sim <- seed_list[s]
+            mr1_recs <- apply(mr1$naa[65:110,1,,,s], 1, sum)
+            # large_event_thresh <- 60
+            large_event_years <- as.numeric(names(which(mr1_recs >= large_event_thresh)))
+            years <- unique(as.vector(sapply(large_event_years, \(x) return(seq(x-2, x+5, 1)))))
+            exp <- 
+                paste0(
+                    "om == '", oname ,
+                    "' & sim == ", sim, 
+                    " & time %in% c(", paste(years, collapse=", "), ")"
+                )
+            rec_years_interest <- c(exp, rec_years_interest)
+        }
+    }
+
+    eval(rec_years_interest[1])
+
+    giant_filter <- rlang::parse_expr(paste(paste0("(", rec_years_interest, ")"), collapse = " | "))
+    return(giant_filter)
+}
+
+#' Average Annual Variation (AAV)
+#' 
+#' Calculates average annual variation ($\frac{\sum{\frac{|x_y - x_{y-1}|}{mean(x)}}}{N-1}$)
+#'
+#' @param data ordered vector of observations of a quantity
+#'
+#' @export aav
+#'
+#' @example
+#'
+aav <- function(data){
+    total <- mean(data)
+    diffs <- abs(diff(data))
+    aav <- sum(diffs/total)/(length(data)-1)
+    return(ifelse(is.nan(aav), 0, aav)) # If all data is 0, return 0 rather than NA
+}
