133847729ad1f738eb1673036f7a728b28891725
Joshua Zahner <joshuazahner@gmail.com>
2025-04-03 16:17:50 -0700

    fix: error in aggregating RPNs and RPWs across areas

--- R/aggregate_observations.R
+++ R/aggregate_observations.R
@@ -17,8 +17,8 @@ aggregate_observations <- function(survey_obs, y){
     out_dims2[4] <- 1
 
     aggregate_obs <- list(
-        rpns = array(apply(survey_obs$rpns, 1, \(x) aggregate_index(x))[1:y], dim=out_dims1),
-        rpws = array(apply(survey_obs$rpws, 1, \(x) aggregate_index(x))[1:y], dim=out_dims1),
+        rpns = array(apply(survey_obs$rpns, c(1, 5), \(x) aggregate_index(x))[1:y,], dim=out_dims1),
+        rpws = array(apply(survey_obs$rpws, c(1, 5), \(x) aggregate_index(x))[1:y,], dim=out_dims1),
         acs = array(apply(survey_obs$acs, c(1, 2, 3, 5), \(x) aggregate_comps(x))[1:y,,,], dim=out_dims2)
     )
 
