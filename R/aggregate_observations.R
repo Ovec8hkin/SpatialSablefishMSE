515fe4866a0e322aad008b9ca42c9cb3a5fefbc3
Joshua Zahner <joshuazahner@gmail.com>
2025-04-24 05:01:21 -0700

    refactor: change `aggregate_observations` to `aggrgeate_indices`
    comp aggregation moved to separate function given inherent complexities associated with weighting

--- R/aggregate_observations.R
+++ R/aggregate_observations.R
@@ -1,27 +1,15 @@
-aggregate_observations <- function(survey_obs, y){
-
-    aggregate_index <- function(index){
-        return(sum(index))
-    }
-
-    aggregate_comps <- function(comps){
-        return(sum(comps))
-    }
+aggregate_indices <- function(survey_obs, y){
 
     out_dims1 <- dim(survey_obs$rpns)
     out_dims1[1] <- y
     out_dims1[4] <- 1
 
-    out_dims2 <- dim(survey_obs$acs)
-    out_dims2[1] <- y
-    out_dims2[4] <- 1
-
     aggregate_obs <- list(
-        rpns = array(apply(survey_obs$rpns, c(1, 5), \(x) aggregate_index(x))[1:y,], dim=out_dims1),
-        rpws = array(apply(survey_obs$rpws, c(1, 5), \(x) aggregate_index(x))[1:y,], dim=out_dims1),
-        acs = array(apply(survey_obs$acs, c(1, 2, 3, 5), \(x) aggregate_comps(x))[1:y,,,], dim=out_dims2)
+        rpns = array(apply(survey_obs$rpns, c(1, 5), \(x) sum(x))[1:y,], dim=out_dims1),
+        rpws = array(apply(survey_obs$rpws, c(1, 5), \(x) sum(x))[1:y,], dim=out_dims1)
     )
 
     return(aggregate_obs)
 
 }
+
