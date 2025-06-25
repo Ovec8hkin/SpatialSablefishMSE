4a9961d683a6d9092f367cad27ab3902a205789d
Joshua Zahner <joshuazahner@gmail.com>
2025-06-25 16:53:31 -0700

    fix: Corrected problem in loading filtered set of saved model runs

--- R/utils.R
+++ R/utils.R
@@ -217,7 +217,7 @@ get_saved_model_runs_old <- function(om_order=NULL, hcr_order=NULL){
 #'
 get_saved_model_runs <- function(om_order=NULL, hcr_order=NULL){
     fs <- list.files(file.path(here::here(), "data", "active"), full.names = TRUE)
-    fs <- sapply(hcr_order, \(x) fs[grepl(sub("/", "", sub(" ", "_", tolower(x))), fs)])
+    fs <- unlist(sapply(hcr_order, \(x) fs[grepl(sub("/", "", sub(" ", "_", tolower(x))), fs)]))
     model_runs <- lapply(seq_along(fs), function(i){
         x <- fs[i]
         m <- readRDS(x)
